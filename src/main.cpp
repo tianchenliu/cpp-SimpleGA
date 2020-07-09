917c653c0d1469089e9a8b41e257d3edae5a7657
Tianchen Liu <tliu.umd@gmail.com>
2020-07-09 05:01:45 -0400

    Slightly revised main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main()
     //ga1.TestIndividualClass();
     ga1.InitPopulation();
 
-    //for (int i=10; i<20; i++) { ga1.pop_gen_prev[i].PrintIndividualInfo(); }
+    for (int i=10; i<20; i++) { ga1.pop_gen_prev[i].PrintIndividualInfo(); }
 
     
     return 0;
